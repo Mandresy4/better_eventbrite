6c2396de805afb6cb31d55d38f78bc48f0ac860e
iroshiva <jean.weil@protonmail.com>
2019-08-12 14:03:24 +0200

    ajout mailer fonctionnel envirronement dév

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
+
+	after_create :welcome_send
+
 	has_many :events, foreign_key: 'admin_id'
 	has_many :attendances
 	has_many :events, through: :attendances
+
+	def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
 
 end

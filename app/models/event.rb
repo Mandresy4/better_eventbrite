26e0b964466a5e27c84849af8fd2ca863b6ae932
iroshiva <jean.weil@protonmail.com>
2019-08-12 12:14:50 +0200

    création model user, event et attendance

---
+++ app/models/event.rb
@@ -0,0 +1,27 @@
+class Event < ApplicationRecord
+
+	belongs_to :admin, class_name: "User"
+	has_many :attendances
+	has_many :users, through: :attendances
+
+	validates :start_date, presence: true, numericality: { greater_than_or_equal_to: DateTime.now.to_i }
+
+	validates :duration, presence: true, numericality: { greater_than: 0 }
+
+	validates :title, presence: true, length: { in: 5..140 }
+
+	validates :description, presence: true, length: { in: 20..1000 }
+
+	validates :price, presence: true, numericality: { only_integer: true, in: 1..1000 }
+
+	validates :location, presence: true
+
+	validate :multiple_of_5?
+
+	def multiple_of_5?
+		if duration%5 != 0
+			errors.add(:duration, "La durée doit être un multiple de 5")
+		end
+	end
+
+end

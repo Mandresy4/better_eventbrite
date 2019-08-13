53c8ed10e1ebfd0fd1fbb5c9ca3afb7c7de08132
iroshiva <jean.weil@protonmail.com>
2019-08-13 18:12:39 +0200

    debug croisement devise et users: mettre devise_for au-dessus de resources:users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
+  
+  # before_action :authenticate_user!, only: [:show]
+  # before_action :connected_user, only: [:show]
+
   def show
+  	puts "#"*60
+  	puts params
+  	puts "#"*60
+  	@user = current_user
   end
+
+  def destroy
+  	puts "#"*60
+  	puts params
+  	puts "#"*60
+  end
+
+  private
+
+  def connected_user
+  	unless current_user == @user = User.find(params[:id])
+  		flash[:danger] = "Tu n'as pas le droit d'aller sur ce profil!"
+      redirect_to events_path
+    end
+  end
+  
 end

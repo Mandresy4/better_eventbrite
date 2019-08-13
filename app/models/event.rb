952087642f44db3099656ae024b431479140663f
iroshiva <jean.weil@protonmail.com>
2019-08-13 15:39:44 +0200

    commit avant créa de controllers et conflits

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,24 +4,39 @@ class Event < ApplicationRecord
 	has_many :attendances
 	has_many :users, through: :attendances
 
-	validates :start_date, presence: true, numericality: { greater_than_or_equal_to: DateTime.now.to_i }
+	validates :start_date, presence: true
 
 	validates :duration, presence: true, numericality: { greater_than: 0 }
 
-	validates :title, presence: true, length: { in: 5..140 }
+	validates :title, presence: true 
+	# length: { in: 5..140 }
 
-	validates :description, presence: true, length: { in: 20..1000 }
+	validates :description, presence: true
+	# , length: { in: 20..1000 }
 
-	validates :price, presence: true, numericality: { in: 1..1000 }
+	validates :price, presence: true
+	# , numericality: { in: 1..1000 }
 
 	validates :location, presence: true
 
 	validate :multiple_of_5?
+	validate :start_date_good?
+
+	private
+
+	def start_date_good?
+		if start_date > Time.now
+			true
+		else
+			errors.add(:start_date, "Ton event doit être dans le futur!")	
+		end
+	end
 
 	def multiple_of_5?
 		if duration%5 != 0
 			errors.add(:duration, "La durée doit être un multiple de 5")
 		end
 	end
+
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-	after_create :welcome_send
+	# after_create :welcome_send
 
 	has_many :events, foreign_key: 'admin_id'
 	has_many :attendances
 	has_many :events, through: :attendances
 
-	def welcome_send
-    UserMailer.welcome_email(self).deliver_now
-  end
+	# def welcome_send
+ #    UserMailer.welcome_email(self).deliver_now
+ #  end
 
 end

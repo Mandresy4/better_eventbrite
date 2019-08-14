02d805bcc8ce721da55a25e25a5af4c71cb0dabc
iroshiva <jean.weil@protonmail.com>
2019-08-14 16:29:32 +0200

    avant stripe

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
 
 
+  get 'attendances/index'
+  get 'attendances/show'
+  get 'attendances/new'
+  get 'attendances/create'
+  get 'attendances/edit'
+  get 'attendances/update'
+  get 'attendances/destroy'
   devise_for :users
   resources :users, only: [:show, :edit, :update]
   resources :events

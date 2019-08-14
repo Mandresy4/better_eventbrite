531e9d38ac58d7d6116cb0a0e779dc438115bc93
iroshiva <jean.weil@protonmail.com>
2019-08-14 23:48:42 +0200

    crea user#edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,15 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
+
+    if @user.update(first_name:params[:first_name], last_name:params[:last_name], description:params[:description])
+
+        flash[:success] = "Ton profil a été édité!"
+        redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
   	
   end
 
